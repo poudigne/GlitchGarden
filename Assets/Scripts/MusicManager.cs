ff6431832bbdebc547045f7027341d5b3d6d3d55
poudigne <pierluc0414@live.ca>
2015-02-17 09:52:47 -0500

    Recommit
    Scene workflow, sound and basic UI stuff

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+  public AudioClip[] levelMusicChangeArray;
+
+
+  private AudioSource audioSource;
+	// Use this for initialization
+	void Awake() {
+    DontDestroyOnLoad(gameObject);
+    Debug.Log("Don't destroy on load : " + name);
+	}
+
+  void Start()
+  {
+    audioSource = GetComponent<AudioSource>();
+  }
+
+  void OnLevelWasLoaded(int level)
+  {
+    AudioClip thisLevelMusic = levelMusicChangeArray[level];
+    Debug.Log("Playing clip : " + thisLevelMusic);
+
+    if (thisLevelMusic) // if there's some music
+    {
+      audioSource.clip = thisLevelMusic;
+      audioSource.loop = true;
+      audioSource.Play();
+    }
+  }
+}
---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashController : MonoBehaviour {
+
+  private int delay = 3;
+
+	// Update is called once per frame
+	void Update () {
+    if (Time.time > delay)
+    {
+      LevelManager man = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+      man.LoadLevel("01 Start Menu");
+    }
+	}
+}
